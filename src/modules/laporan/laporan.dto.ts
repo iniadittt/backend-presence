792e2cc87452937ecdff0dc30cc416ad2a3e335a
iniadittt <adityabayu861@gmail.com>
2024-07-14 17:19:41 +0700

    first commit

--- src/modules/laporan/laporan.dto.ts
+++ src/modules/laporan/laporan.dto.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
 export const addSchema = z.object({
-    title: z.string().min(8).max(255),
-    description: z.string().min(8).max(1000)
+    title: z.string().min(1).max(255),
+    description: z.string().min(1).max(1000)
 })
