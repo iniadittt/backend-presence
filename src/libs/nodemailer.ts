ac0b63df2b83a625813d40ee999e373b5732bfb6
iniadittt <adityabayu861@gmail.com>
2024-07-18 06:04:38 +0700

    update

--- src/libs/nodemailer.ts
+++ src/libs/nodemailer.ts
@@ -3,6 +3,7 @@ import { environment } from "../common/environment"
 import nodemailer from "nodemailer"
 import SMTPTransport from "nodemailer/lib/smtp-transport"
 
+console.log({ environment })
 const oAuth2Client = new google.auth.OAuth2(
     environment.mail.client_id,
     environment.mail.client_secret,
@@ -12,35 +13,35 @@ const oAuth2Client = new google.auth.OAuth2(
 oAuth2Client.setCredentials({ refresh_token: environment.mail.refresh_token })
 
 const sendEmail = async (to: string, subject: string, html: string) => {
-    const ACCESS_TOKEN = await oAuth2Client.getAccessToken()
-    const transportOptions: SMTPTransport.Options = {
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: environment.mail.email,
-            clientId: environment.mail.client_id,
-            clientSecret: environment.mail.client_secret,
-            refreshToken: environment.mail.refresh_token,
-            accessToken: ACCESS_TOKEN.token?.toString(),
-        },
-        tls: {
-            rejectUnauthorized: true,
+    try {
+        const { token: ACCESS_TOKEN } = await oAuth2Client.getAccessToken()
+        if (!ACCESS_TOKEN) throw new Error("Failed to retrieve access token")
+        const transportOptions: SMTPTransport.Options = {
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: environment.mail.email,
+                clientId: environment.mail.client_id,
+                clientSecret: environment.mail.client_secret,
+                refreshToken: environment.mail.refresh_token,
+                accessToken: ACCESS_TOKEN,
+            },
+            tls: {
+                rejectUnauthorized: true,
+            }
         }
-    };
-    const transport = nodemailer.createTransport(transportOptions);
-    const mailOptions = {
-        from: `Presence <${environment.mail.email}>`,
-        to,
-        subject,
-        html
+        const transport = nodemailer.createTransport(transportOptions)
+        const mailOptions = {
+            from: `Presence Admin`,
+            to,
+            subject,
+            html
+        }
+        const info = await transport.sendMail(mailOptions)
+        return info
+    } catch (err) {
+        throw new Error(`Error sending email: ${err}`)
     }
-    return new Promise((resolve, reject) => {
-        transport.sendMail(mailOptions, (err, info) => {
-            console.log({ err, info })
-            if (err) reject(err)
-            resolve(info)
-        })
-    })
 }
 
 export default sendEmail
